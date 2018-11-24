c6759bd7e7b6e3febf77c153e8a8729e4033fe16
John Ray <john@johnraymon.com>
2018-11-24 13:38:51 -0500

    create homepage ui

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module DeleteMyTweets
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators.assets = false
+
+    config.exceptions_app = self.routes
+
+    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
